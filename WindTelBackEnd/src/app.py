c7a2eb24b24c0bda5647f53746d0e9247fd10d49
kahlil-14 <kahlil.fonseca@upr.edu>
2019-04-03 12:05:10 -0400

    app.py update
    login, register, and getAllUsers methods have been added

--- WindTelBackEnd/src/app.py
+++ WindTelBackEnd/src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from handler.researcher import ResearcherHandler
 from flask_cors import CORS
 
 #Activate
@@ -7,8 +8,31 @@ CORS(app)
 
 # Default route
 @app.route('/')
+@app.route('/WindTel/')
 def greeting():
     return 'This is the opening page of the WindTel application'
+
+#Registration
+@app.route('/WindTel/register/', methods=['GET', 'POST'])
+def registerResearcher():
+    if request.method == 'POST':
+        print("REQUEST: ", request.json)
+        return ResearcherHandler().registerResearcher(request.json)
+    else:
+        return jsonify(Error="Method not allowed."), 405
+
+#Login
+@app.route('/WindTel/login/', methods=['GET', 'POST'])
+def loginResearcher():
+    if request.method == 'POST':
+        return ResearcherHandler().loginResearcher(request.json)
+    else:
+        return jsonify(Error="Method not allowed."), 405
+
+#All users
+@app.route('/WindTel/users')
+def getAllUsers():
+    return ResearcherHandler().getAllUsers()
 
 if __name__ == '__main__':
     app.run(debug=True)
