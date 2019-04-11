2a9d84be6780ce91ddeaec3688ebbed190cea476
yldas <misael.valentin@upr.edu>
2019-04-11 05:10:49 -0400

    added results page

--- windtel_app/src/app/tab2/tab2.page.ts
+++ windtel_app/src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+
 import { Experiment } from '../experiment';
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-tab2',
@@ -8,9 +11,22 @@ import { Experiment } from '../experiment';
 })
 export class Tab2Page {
 
+  currentUser: User;
+  permissions: string;
+
   model = new Experiment(1, 'my_first_experiment', 'This is my first experiment!', '15 seconds', '4/11/2019', 1, 10);
+
+  constructor(private userService: UserService) { }
+
+  getCurrentUser() {
+    this.currentUser = this.userService.getCurrentUser();
+    this.permissions = this.currentUser.permissions;
+  }
 
   tryExperimentStart() {
     // TODO
   }
+
+  ngOnInit() { }
+
 }
--- windtel_app/src/app/tab3/tab3.page.ts
+++ windtel_app/src/app/tab3/tab3.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
 
+import { UserService } from '../user.service';
+import { User } from '../user';
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+  currentUser: User;
+  permissions: string;
+  
+  constructor(private userService: UserService) { }
+
+  getCurrentUser() {
+    this.currentUser = this.userService.getCurrentUser();
+    this.permissions = this.currentUser.permissions;
+  }
+
+}
